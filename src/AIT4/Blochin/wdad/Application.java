f94e9fff79ee06b6935d608abf669f1e39fb366c
Dmitrey Blochin <dmitreycogito@gmail.com>
2019-03-22 22:16:27 +0300

    Added entry point

--- src/AIT4/Blochin/wdad/Application.java
+++ src/AIT4/Blochin/wdad/Application.java
@@ -1,4 +1,7 @@
 package AIT4.Blochin.wdad;
 
 public class Application {
+    public static void main(String[] args){
+        System.out.println("I’m Dmitrey Blochin, and I’m not a monkey");
+    }
 }
