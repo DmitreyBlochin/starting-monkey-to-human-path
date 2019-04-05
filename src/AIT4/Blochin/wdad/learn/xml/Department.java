da863f322e2c660540d446869adf4d9f1829a2b4
Dmitrey Blochin <dmitreycogito@gmail.com>
2019-04-06 03:50:44 +0500

    added java-class Department

---
+++ src/AIT4/Blochin/wdad/learn/xml/Department.java
@@ -0,0 +1,22 @@
+package AIT4.Blochin.wdad.learn.xml;
+
+import java.util.ArrayList;
+
+public class Department {
+    private ArrayList<Employee> emplsList;
+    private String name;
+
+
+    public Department(String name){
+        emplsList = new ArrayList<Employee>();
+        this.name = name;
+    }
+
+    public ArrayList<Employee> getEmplsList() {
+        return emplsList;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
