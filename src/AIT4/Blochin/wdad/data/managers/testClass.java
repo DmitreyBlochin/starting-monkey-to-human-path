0683dda68914e8656bcb18a683fc89be205029f0
Dmitrey Blochin <dmitreycogito@gmail.com>
2019-04-12 21:44:48 +0500

    added singleton class PreferencesManager

---
+++ src/AIT4/Blochin/wdad/data/managers/testClass.java
@@ -0,0 +1,17 @@
+package AIT4.Blochin.wdad.data.managers;
+
+import org.w3c.dom.NodeList;
+
+public class testClass {
+
+    public static void main(String[] args) {
+
+        PreferencesManager pm = PreferencesManager.getInstance();
+
+//        System.out.println(pm.getClassprovider());
+//        pm.setClassprovider("http://172.0.0.1");
+//        System.out.println(pm.getClassprovider());
+
+
+    }
+}
