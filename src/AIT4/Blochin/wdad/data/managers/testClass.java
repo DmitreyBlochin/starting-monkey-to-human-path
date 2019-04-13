fd3b1dd3e6c361243d8c6d6807add4399b137445
Dmitrey Blochin <dmitreycogito@gmail.com>
2019-04-13 08:00:43 +0500

    tested class PreferencesManager

--- src/AIT4/Blochin/wdad/data/managers/testClass.java
+++ src/AIT4/Blochin/wdad/data/managers/testClass.java
@@ -8,9 +8,29 @@ public class testClass {
 
         PreferencesManager pm = PreferencesManager.getInstance();
 
-//        System.out.println(pm.getClassprovider());
-//        pm.setClassprovider("http://172.0.0.1");
-//        System.out.println(pm.getClassprovider());
+        System.out.println(pm.getCreateregistry());
+        pm.setCreateregistry("no");
+        System.out.println(pm.getCreateregistry());
+
+        System.out.println(pm.getRegistryaddress());
+        pm.setRegistryaddress("172.0.0.1");
+        System.out.println(pm.getRegistryaddress());
+
+        System.out.println(pm.getRegistryport());
+        pm.setRegistryport("2020");
+        System.out.println(pm.getRegistryport());
+
+        System.out.println(pm.getPolicypath());
+        pm.setPolicypath("admin.policy");
+        System.out.println(pm.getPolicypath());
+
+        System.out.println(pm.getUsecodebaseonly());
+        pm.setUsecodebaseonly("yes");
+        System.out.println(pm.getUsecodebaseonly());
+
+        System.out.println(pm.getClassprovider());
+        pm.setClassprovider("http://www.randomhost.com");
+        System.out.println(pm.getClassprovider());
 
 
     }
